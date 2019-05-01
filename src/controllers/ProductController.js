e125d9f22e243f53864a460eab0ee06429c2951e
Samuel Hiroyuki <36797629+SamuelHiroyuki@users.noreply.github.com>
2019-05-01 14:38:36 -0300

    Finalização do CRUD

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -3,7 +3,40 @@ const Product = require("../models/Product");
 class ProductController {
     async get(req, res) {
         const products = await Product.find();
+
         return res.json(products);
+    }
+
+    async getById(req, res) {
+        const product = await Product.findById(req.params.id);
+
+        return res.json(product);
+    }
+
+    async post(req, res) {
+        const product = await Product.create({
+            title: req.body.title,
+            description: req.body.description,
+            url: req.body.url
+        });
+
+        return res.json(product);
+    }
+
+    async put(req, res) {
+        const product = await Product.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+
+        return res.json(product);
+    }
+
+    async delete(req, res) {
+        await Product.findByIdAndRemove(req.params.id);
+
+        return res.send();
     }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,9 @@ const routes = express.Router();
 const ProductController = require("./controllers/ProductController");
 
 routes.get("/Products", ProductController.get);
+routes.get("/Products/:id", ProductController.getById);
+routes.post("/Products", ProductController.post);
+routes.put("/Products/:id", ProductController.put);
+routes.delete("/Products/:id", ProductController.delete);
 
 module.exports = routes;

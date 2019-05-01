493e3d9f0124226df4d084758a99fb4c665a3915
Samuel Hiroyuki <36797629+SamuelHiroyuki@users.noreply.github.com>
2019-05-01 13:45:07 -0300

    Product Model and MongoDB connection

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 const app = express();
 
+//Iniciando o DB
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+    useNewUrlParser: true
+});
+
+requireDir("./src/models");
+
+const Product = mongoose.model("Product");
+
 app.get("/", (req, res) => {
+    Product.create({
+        title: "Produto 1",
+        description: "Descrição",
+        url: "http://github.com/SamuelHiroyuki/rs-starter-nodejs"
+    });
     return res.send("Hello World");
 });
 
